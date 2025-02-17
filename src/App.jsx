29befa44f87b906c3a00f590daa65b1408839a37
Khanh <khanh.qn.developer@gmail.com>
2025-02-17 12:04:02 +0700

    feat: main content UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import MainContent from "./components/MainContent.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
     return (
         <div id="app">
             <Navbar />
+            <MainContent />
         </div>
     );
 }
