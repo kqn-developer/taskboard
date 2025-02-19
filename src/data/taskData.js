15d2606eeb6374b4f3514cc5e4c3d5d26ea1765c
Khanh <khanh.qn.developer@gmail.com>
2025-02-19 15:02:56 +0700

    feat: add task UI

--- src/data/taskData.js
+++ src/data/taskData.js
@@ -1,5 +1,16 @@
 export const taskTypes = [
     { id: 1, type: "todo", displayName: "Việc cần làm", bgColor: "bg-gray-300" },
-    { id: 2, type: "doing", displayName: "Đang làm", bgColor:"bg-blue-300" },
-    { id: 3, type: "done", displayName: "Hoàn thành", bgColor:"bg-green-300" },
+    { id: 2, type: "doing", displayName: "Đang làm", bgColor: "bg-blue-300" },
+    { id: 3, type: "done", displayName: "Hoàn thành", bgColor: "bg-green-300" },
+];
+
+export const priorities = [
+    { id: 1, name: "Ưu tiên 1", color: "text-red-500", slug: "uu-tien-1" },
+    { id: 2, name: "Ưu tiên 2", color: "text-yellow-500", slug: "uu-tien-2" },
+    { id: 3, name: "Ưu tiên 3", color: "text-blue-500", slug: "uu-tien-3" },
+];
+
+export const labels = [
+    { id: 1, slug: "hoc-tap", name: "Học tập" },
+    { id: 2, slug: "lam-viec", name: "Làm việc" },
 ];
